d7eca3d6c32ae8a14690e915ca9927e43a30994e
illyum <90023277+itzilly@users.noreply.github.com>
2023-03-14 05:12:51 -0600

    Generate configuration file if it does not exist

--- Dragon Studios bot.py
+++ Dragon Studios bot.py
@@ -1,8 +1,14 @@
+import os
 import json
 import discord
 from discord.utils import get
 from discord.ext import commands
 
+CONFIG_PATH = 'settings.json'
+
+if not os.path.isfile(CONFIG_PATH):
+    with open(CONFIG_PATH, 'w') as config_file:
+        config_file.write('{"token": ""}')
 
 with open('settings.json', 'r') as token_file:
     data = json.load(token_file)
